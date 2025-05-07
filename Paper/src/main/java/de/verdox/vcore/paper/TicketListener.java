28eea767847af37454b850b281e12c83de4d95bf
verdox <mail.ysp@web.de>
2025-05-07 20:56:48 +0000

    Add caching

--- Paper/src/main/java/de/verdox/vcore/paper/TicketListener.java
+++ Paper/src/main/java/de/verdox/vcore/paper/TicketListener.java
@@ -9,22 +9,37 @@ import org.bukkit.event.player.AsyncPlayerPreLoginEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
+
 public class TicketListener implements Listener {
     private final JavaPlugin javaPlugin;
     private final NetworkParticipant networkParticipant;
+
+    private final Set<UUID> potential = ConcurrentHashMap.newKeySet();
+
     public TicketListener(JavaPlugin javaPlugin, NetworkParticipant networkParticipant) {
         this.javaPlugin = javaPlugin;
         this.networkParticipant = networkParticipant;
     }
 
     @EventHandler
-    public void preloadDataOnConnect(AsyncPlayerPreLoginEvent e){
+    public void preloadDataOnConnect(AsyncPlayerPreLoginEvent e) {
         networkParticipant.messagingService().getTicketPropagator().triggerTicketDataPreloadGroup(PlayerConnectionTicket.class);
+        potential.add(e.getUniqueId());
     }
+
     @EventHandler
     public void consumeTicketsOnJoin(PlayerJoinEvent e) {
+        if(!potential.contains(e.getPlayer().getUniqueId())) {
+            return;
+        }
         Bukkit.getScheduler().runTaskLater(javaPlugin, () -> {
             networkParticipant.messagingService().getTicketPropagator().consumeTicketGroup(PlayerConnectionTicket.class, e.getPlayer());
         }, 20L);
+        potential.remove(e.getPlayer().getUniqueId());
     }
 }
